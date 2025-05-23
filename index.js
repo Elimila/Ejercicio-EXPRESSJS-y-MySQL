d2b67bb5420fc6cda274b4dc124717407af2d97b
unknown <elimila.rodriguez@gmail.com>
2025-05-23 23:44:06 +0200

    endpoint mostrar Ej4

--- index.js
+++ index.js
@@ -81,6 +81,120 @@ app.get('/categories', (req, res) => {
   })
 })
 
+// Ejercicio 3
+
+app.put('/updateProduct/:id', (req, res) => {
+  const { name, type, quantity, precio } = req.body
+  const id = req.params.id
+
+  const sql = `
+    UPDATE products 
+    SET name = ?, type = ?, quantity = ?, precio = ? 
+    WHERE id = ?
+  `
+
+  db.query(sql, [name, type, quantity, precio, id], (err, result) => {
+    if (err) throw err
+    res.send('Producto actualizado')
+  })
+})
+
+app.put('/updateCategory/:id', (req, res) => {
+  const { name } = req.body
+  const id = req.params.id
+
+  const sql = 'UPDATE categories SET name = ? WHERE id = ?'
+
+  db.query(sql, [name, id], (err, result) => {
+    if (err) throw err
+    res.send('Categoría actualizada')
+  })
+})
+
+
+// Ejercicio 4
+
+// Mostrar todos los productos
+app.get('/products', (req, res) => {
+  const sql = 'SELECT * FROM products'
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
+})
+
+// Mostrar todas las categorías
+app.get('/categories', (req, res) => {
+  const sql = 'SELECT * FROM categories'
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
+})
+
+// Mostrar productos con sus categorías (relación no implementada aún)
+app.get('/products-with-categories', (req, res) => {
+  const sql = `
+    SELECT products.id, products.name AS product_name, products.type, products.quantity, products.precio,
+           categories.name AS category_name
+    FROM products
+    LEFT JOIN categories ON products.category_id = categories.id
+  `
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
+})
+// ⚠️ Este endpoint funcionará solo si luego agregamos la columna category_id a la tabla products
+
+// Mostrar producto por ID
+app.get('/product/:id', (req, res) => {
+  const { id } = req.params
+  const sql = 'SELECT * FROM products WHERE id = ?'
+  db.query(sql, [id], (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
+})
+
+// Mostrar productos en orden descendente
+app.get('/products-desc', (req, res) => {
+  const sql = 'SELECT * FROM products ORDER BY id DESC'
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
+})
+
+// Mostrar categoría por ID
+app.get('/category/:id', (req, res) => {
+  const { id } = req.params
+  const sql = 'SELECT * FROM categories WHERE id = ?'
+  db.query(sql, [id], (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
+})
+
+// Buscar producto por nombre
+app.get('/searchProduct/:name', (req, res) => {
+  const { name } = req.params
+  const sql = 'SELECT * FROM products WHERE name LIKE ?'
+  db.query(sql, [`%${name}%`], (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
+})
+
+// Eliminar producto por ID
+app.delete('/deleteProduct/:id', (req, res) => {
+  const { id } = req.params
+  const sql = 'DELETE FROM products WHERE id = ?'
+  db.query(sql, [id], (err, result) => {
+    if (err) throw err
+    res.send('Producto eliminado')
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
