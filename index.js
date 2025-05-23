643633e37d6f626bed2894854e29fd12460f61d0
unknown <elimila.rodriguez@gmail.com>
2025-05-23 22:06:32 +0200

    table categories

--- index.js
+++ index.js
@@ -24,6 +24,27 @@ app.get('/createdb', (req, res) => {
  })
 })
 
+app.get('/createTableProducts', (req, res) => {
+ const sql =
+   'CREATE TABLE products(id int AUTO_INCREMENT,name VARCHAR(255), type VARCHAR(50), quantity INT, precio FLOAT,  PRIMARY KEY(id))'
+ db.query(sql, (err, result) => {
+   if (err) throw err
+   console.log(result)
+   res.send('Tabla products creada...')
+ })
+})
+
+
+app.get('/createTableCategories', (req, res) => {
+ const sql =
+   'CREATE TABLE categories(id INT AUTO_INCREMENT,name VARCHAR(255), PRIMARY KEY(id))'
+ db.query(sql, (err, result) => {
+   if (err) throw err
+   res.send('Tabla categories creada...')
+ })
+})
+
+
 app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
