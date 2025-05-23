407669264af7c04352fc7390acd57b15e9443fe2
unknown <elimila.rodriguez@gmail.com>
2025-05-23 23:07:58 +0200

    con postman se agrego produc y catego..

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const db = mysql.createConnection({
 
 db.connect()
 
+// Ejercicio1 
 
 app.get('/createdb', (req, res) => {
  const sql = 'CREATE DATABASE expressSqlDB'
@@ -42,6 +43,42 @@ app.get('/createTableCategories', (req, res) => {
    if (err) throw err
    res.send('Tabla categories creada...')
  })
+})
+
+// Ejercicio 2
+
+app.post('/addProduct', (req, res) => {
+  const { name, type, quantity, precio } = req.body
+  const sql = 'INSERT INTO products (name, type, quantity, precio) VALUES (?, ?, ?, ?)'
+  db.query(sql, [name, type, quantity, precio], (err, result) => {
+    if (err) throw err
+    res.send('Producto añadido')
+  })
+})
+
+app.post('/addCategory', (req, res) => {
+  const { name } = req.body
+  const sql = 'INSERT INTO categories (name) VALUES (?)'
+  db.query(sql, [name], (err, result) => {
+    if (err) throw err
+    res.send('Categoría añadida')
+  })
+})
+
+app.get('/products', (req, res) => {
+  const sql = 'SELECT * FROM products'
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
+})
+
+app.get('/categories', (req, res) => {
+  const sql = 'SELECT * FROM categories'
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    res.send(result)
+  })
 })
 
 
